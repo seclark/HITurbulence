07a5f2138edfb65300e073588bcdcdeff85c6aee
Susan Clark <susanclark19@gmail.com>
2016-01-17 22:12:59 -0500

    plotting

--- code/GALFA_SF.py
+++ code/GALFA_SF.py
@@ -11,9 +11,11 @@ from scipy import ndimage as ndi
 # HI DATA
 galfa_data_fn = "/Users/susanclark/Dropbox/GALFA-Planck/Big_Files/SC_241.66_28.675.best_20.fits"
 galfa_data = fits.getdata(galfa_data_fn)
-no_fluct_data = galfa_data - np.nanmean(galfa_data)
 dx = 512
-no_fluct_data = no_fluct_data[500:500+dx, 4800:4800+dx]
+galfa_data = galfa_data[500:500+dx, 4800:4800+dx]
+no_fluct_data = galfa_data - np.nanmean(galfa_data)
+
+#no_fluct_data = no_fluct_data[500:500+dx, 4800:4800+dx]
 
 # B-field data
 QU = fits.getdata("/Volumes/DataDavy/Planck/SC_241.66_28.675.allBWmustdie.PlanckQU_Equi_IAU.fits")
@@ -22,7 +24,7 @@ U = QU[1, :, :].T
 theta_353 = np.mod(0.5*np.arctan2(-U, -Q), np.pi)
 theta_353_chunk = theta_353[500:500+dx, 4800:4800+dx]
 
-def cut_out_SF(data, x0, y0, rad, mirror = True):
+def cut_out_SF(data, x0, y0, rad, mirror = True, square = False):
     # Cut out an annulus
     mnvals = np.indices(data.shape)
     mvals = mnvals[:, :][0] # These are the y points
@@ -30,8 +32,11 @@ def cut_out_SF(data, x0, y0, rad, mirror = True):
     rads = np.zeros(data.shape, np.float_)
     rads = np.sqrt((mvals-y0)**2 + (nvals-x0)**2)
     
-    data[rads >= rad] = 0
-    #data = data[y0-rad:y0+rad+1, x0-rad:x0+rad+1]
+    if square == False:
+        data[rads >= rad] = 0
+    data = data[y0-rad:y0+rad+1, x0-rad:x0+rad+1]
+    
+    data = data - np.nanmean(data)
     
     FT = scipy.fftpack.fft2(data)
     AC = (scipy.fftpack.ifft2(FT * np.conjugate(FT))).real
@@ -42,28 +47,31 @@ def cut_out_SF(data, x0, y0, rad, mirror = True):
     
     return data, SF
     
-rad = 100
+rad = 150
 x0 = 300
 y0 = 300
-SF_chunk = galfaSF[x0-rad:x0+rad, y0-rad:y0+rad]
+#SF_chunk = galfaSF[x0-rad:x0+rad, y0-rad:y0+rad]
 
-data, cc_sf = cut_out_SF(no_fluct_data, x0, y0, rad)
+data, cc_sf = cut_out_SF(no_fluct_data, x0, y0, rad, square = True)
+datay, datax = data.shape
+print(datay, datax)
 
-rollcc = np.roll(np.roll(cc_sf, dx/2, axis=1), dx/2, axis=0)
-rotrollcc = ndi.interpolation.rotate(cc_sf, np.degrees(theta_353_chunk[x0, y0]), reshape=False)
+rollcc = np.roll(np.roll(cc_sf, datax/2, axis=1), datax/2, axis=0)
+rotrollcc = ndi.interpolation.rotate(rollcc, np.degrees(theta_353_chunk[x0, y0]), reshape=False)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(221)
 ax2 = fig.add_subplot(222)
 ax3 = fig.add_subplot(223)
-ax3 = fig.add_subplot(224)
+ax4 = fig.add_subplot(224)
 
 ax1.imshow(data)
 ax2.imshow(cc_sf)
 ax3.imshow(rotrollcc)
 
-ax4.plot(rotrollcc[dx/2, dx/2:] - np.nanmin(rotrollcc), ".", color = "red")
-ax4.plot(rotrollcc[dx/2:, dx/2] - np.nanmin(rotrollcc), ".", color = "blue")
+ax4.loglog(rotrollcc[datax/2, datax/2:] - np.nanmin(rotrollcc), ".", color = "red")
+ax4.loglog(rotrollcc[datax/2:, datax/2] - np.nanmin(rotrollcc), ".", color = "blue")
+ax4.set_xlim(10, rad)
 
 """
 radius = 150
